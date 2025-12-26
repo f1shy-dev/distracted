70c2f6b58b85d4a0d9a9adcd71a6b450dc072f38
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2025-12-26 22:20:11 +0000

    Get tailwind working

---
+++ src/entrypoints/content/App.tsx
@@ -0,0 +1,6 @@
+import { ComponentExample } from "@/components/component-example";
+
+export default () => {
+
+  return <ComponentExample />;
+};
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   srcDir: "src",
+  manifest: {
+    name: "distracted",
+    description: "Prevent yourself from being distracted by websites",
+    permissions: ["webNavigation", "storage", "tabs", "activeTab"],
+    host_permissions: ["<all_urls>"],
+  },
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }),
 });
